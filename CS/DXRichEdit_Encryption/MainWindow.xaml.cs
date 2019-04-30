17421922711b0fe48b912f30593c538bc770c5ea
Poline Fedorova <polina.fedorova@devexpress.com>
2019-04-30 14:04:21 +0300

    add files

---
+++ CS/DXRichEdit_Encryption/MainWindow.xaml.cs
@@ -0,0 +1,116 @@
+﻿using DevExpress.Xpf.Core;
+using DevExpress.Xpf.Editors;
+using DevExpress.Xpf.RichEdit;
+using DevExpress.XtraRichEdit;
+using DevExpress.XtraRichEdit.API.Native;
+using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Forms;
+
+namespace DXRichEdit_Encryption
+{
+    public partial class MainWindow : ThemedWindow
+    {
+        int tryCount = 4;
+        RichEditDocumentSource source;
+        public MainWindow()
+        {
+            DataContext = this;
+            source = new RichEditDocumentSource("FirstLook.docx", DocumentFormat.OpenXml);
+            InitializeComponent();
+            RichEditDocumentSource = source;
+            InitializeEncryptionOptions();
+        }
+        public RichEditDocumentSource RichEditDocumentSource
+        {
+            get { return source; }
+            private set { source = value; }
+        }
+
+        void InitializeEncryptionOptions()
+        {
+            passwordEdit.Text = "test";
+
+            string[] array = Enum.GetNames(typeof(EncryptionType));
+            encryptionComboBoxEdit.ItemsSource = array;
+            encryptionComboBoxEdit.SelectedItem = EncryptionType.Strong.ToString();
+        }
+
+
+        void SaveAs(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.SaveFileDialog sfDialog = new Microsoft.Win32.SaveFileDialog();
+            sfDialog.Filter = "Word 2007 Document (*.docx)|*.docx|Microsoft Word Document (*.doc*)|*.doc*";
+            bool? result = sfDialog.ShowDialog();
+            if (!result.HasValue || !result.Value)
+                return;
+            else
+            {
+                string ext = Path.GetExtension(sfDialog.FileName);
+                if (ext == ".docx")
+                    richEditControl1.SaveDocument(sfDialog.FileName, DocumentFormat.OpenXml);
+                else
+                    richEditControl1.SaveDocument(sfDialog.FileName, DocumentFormat.Doc);
+            }
+
+            string fileName = sfDialog.FileName;
+
+            if (openFileCheckEditBox.IsChecked.HasValue && openFileCheckEditBox.IsChecked.Value)
+                richEditControl1.LoadDocument(fileName);
+        }
+
+
+        void passwordChanged(object sender, EditValueChangedEventArgs e)
+        {
+            richEditControl1.Document.Encryption.Password = passwordEdit.Text;
+        }
+
+        private void EncryptionComboBoxEdit_EditValueChanged(object sender, EditValueChangedEventArgs e)
+        {
+            richEditControl1.Document.Encryption.Type = (EncryptionType)Enum.Parse(typeof(EncryptionType), encryptionComboBoxEdit.Text);
+        }
+
+        private void RichEditControl1_EncryptedFilePasswordRequested(object sender, EncryptedFilePasswordRequestedEventArgs e)
+        {
+            //Count the amount of attempts to enter the password
+            if (tryCount > 0)
+            {
+                tryCount--;
+            }
+        }
+
+        private void RichEditControl1_EncryptedFilePasswordCheckFailed(object sender, EncryptedFilePasswordCheckFailedEventArgs e)
+        {
+            //Analyze the error led to this event
+            switch (e.Error)
+            {
+                case RichEditDecryptionError.PasswordRequired:
+                    if (tryCount > 0)
+                    {
+                        e.TryAgain = true;
+                        e.Handled = true;
+                        System.Windows.Forms.MessageBox.Show("You did not enter the password!", String.Format(" {0} attempts left", tryCount));
+                    }
+                    else
+                        e.TryAgain = false;
+
+                    break;
+
+                case RichEditDecryptionError.WrongPassword:
+                    if (tryCount > 0)
+                    {
+                        if (System.Windows.Forms.MessageBox.Show("The password is incorrect. Try Again?", string.Format("{0} attempts left", tryCount),
+                            MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == System.Windows.Forms.DialogResult.Yes)
+                        {
+                            e.TryAgain = true;
+                            e.Handled = true;
+                        }
+                    }
+                    break;
+            }
+
+        }
+    }
+
+}
---
+++ VB/DXRichEdit_Encryption/MainWindow.xaml.vb
@@ -0,0 +1,105 @@
+﻿Imports DevExpress.Xpf.Core
+Imports DevExpress.Xpf.Editors
+Imports DevExpress.Xpf.RichEdit
+Imports DevExpress.XtraRichEdit
+Imports DevExpress.XtraRichEdit.API.Native
+Imports System
+Imports System.IO
+Imports System.Windows
+Imports System.Windows.Forms
+
+Namespace DXRichEdit_Encryption
+	Partial Public Class MainWindow
+		Inherits ThemedWindow
+
+		Private tryCount As Integer = 4
+		Private source As RichEditDocumentSource
+		Public Sub New()
+			DataContext = Me
+			source = New RichEditDocumentSource("FirstLook.docx", DocumentFormat.OpenXml)
+			InitializeComponent()
+			RichEditDocumentSource = source
+			InitializeEncryptionOptions()
+		End Sub
+		Public Property RichEditDocumentSource() As RichEditDocumentSource
+			Get
+				Return source
+			End Get
+			Private Set(ByVal value As RichEditDocumentSource)
+				source = value
+			End Set
+		End Property
+
+		Private Sub InitializeEncryptionOptions()
+			passwordEdit.Text = "test"
+
+			Dim array() As String = System.Enum.GetNames(GetType(EncryptionType))
+			encryptionComboBoxEdit.ItemsSource = array
+			encryptionComboBoxEdit.SelectedItem = EncryptionType.Strong.ToString()
+		End Sub
+
+
+		Private Sub SaveAs(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			Dim sfDialog As New Microsoft.Win32.SaveFileDialog()
+			sfDialog.Filter = "Word 2007 Document (*.docx)|*.docx|Microsoft Word Document (*.doc*)|*.doc*"
+			Dim result? As Boolean = sfDialog.ShowDialog()
+			If (Not result.HasValue) OrElse (Not result.Value) Then
+				Return
+			Else
+				Dim ext As String = Path.GetExtension(sfDialog.FileName)
+				If ext = ".docx" Then
+					richEditControl1.SaveDocument(sfDialog.FileName, DocumentFormat.OpenXml)
+				Else
+					richEditControl1.SaveDocument(sfDialog.FileName, DocumentFormat.Doc)
+				End If
+			End If
+
+			Dim fileName As String = sfDialog.FileName
+
+			If openFileCheckEditBox.IsChecked.HasValue AndAlso openFileCheckEditBox.IsChecked.Value Then
+				richEditControl1.LoadDocument(fileName)
+			End If
+		End Sub
+
+
+		Private Sub passwordChanged(ByVal sender As Object, ByVal e As EditValueChangedEventArgs)
+			richEditControl1.Document.Encryption.Password = passwordEdit.Text
+		End Sub
+
+		Private Sub EncryptionComboBoxEdit_EditValueChanged(ByVal sender As Object, ByVal e As EditValueChangedEventArgs)
+			richEditControl1.Document.Encryption.Type = DirectCast(System.Enum.Parse(GetType(EncryptionType), encryptionComboBoxEdit.Text), EncryptionType)
+		End Sub
+
+		Private Sub RichEditControl1_EncryptedFilePasswordRequested(ByVal sender As Object, ByVal e As EncryptedFilePasswordRequestedEventArgs)
+			'Count the amount of attempts to enter the password
+			If tryCount > 0 Then
+				tryCount -= 1
+			End If
+		End Sub
+
+		Private Sub RichEditControl1_EncryptedFilePasswordCheckFailed(ByVal sender As Object, ByVal e As EncryptedFilePasswordCheckFailedEventArgs)
+			'Analyze the error led to this event
+			Select Case e.Error
+				Case RichEditDecryptionError.PasswordRequired
+					If tryCount > 0 Then
+						e.TryAgain = True
+						e.Handled = True
+						System.Windows.Forms.MessageBox.Show("You did not enter the password!", String.Format(" {0} attempts left", tryCount))
+					Else
+						e.TryAgain = False
+					End If
+
+
+				Case RichEditDecryptionError.WrongPassword
+					If tryCount > 0 Then
+						If System.Windows.Forms.MessageBox.Show("The password is incorrect. Try Again?", String.Format("{0} attempts left", tryCount), MessageBoxButtons.YesNo, MessageBoxIcon.Warning) = System.Windows.Forms.DialogResult.Yes Then
+							e.TryAgain = True
+							e.Handled = True
+						End If
+					End If
+			End Select
+
+		End Sub
+	End Class
+
+End Namespace
